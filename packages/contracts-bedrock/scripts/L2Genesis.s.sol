c0f538d8deaa73bd2c5e783a101309c60df06de3
Wyatt Barnes <me@wyatt.email>
2024-02-06 18:16:21 -1000

    WIP

--- packages/contracts-bedrock/scripts/L2Genesis.s.sol
+++ packages/contracts-bedrock/scripts/L2Genesis.s.sol
@@ -5,7 +5,7 @@ import { Script } from "forge-std/Script.sol";
 import { console2 as console } from "forge-std/console2.sol";
 
 import { Artifacts } from "scripts/Artifacts.s.sol";
-import { DeployConfig } from "scripts/DeployConfig.s.sol";
+import { DeployConfig, DEV_ACCOUNT_FUND_AMT } from "scripts/DeployConfig.s.sol";
 import { Predeploys } from "src/libraries/Predeploys.sol";
 import { L1StandardBridge } from "src/L1/L1StandardBridge.sol";
 import { L1CrossDomainMessenger } from "src/L1/L1CrossDomainMessenger.sol";
@@ -17,6 +17,7 @@ import { OptimismMintableERC20Factory } from "src/universal/OptimismMintableERC2
 import { L1Block } from "src/L2/L1Block.sol";
 import { GovernanceToken } from "src/governance/GovernanceToken.sol";
 import { EIP1967Helper } from "test/mocks/EIP1967Helper.sol";
+import { L2GenesisHelpers } from "scripts/libraries/L2GenesisHelpers.sol";
 
 interface IInitializable {
     function initialize(address _addr) external;
@@ -46,33 +47,8 @@ interface IInitializable {
 ///         expected to be set after a new deployment. Lastly, we reset the account code and storage slots the contract
 ///         was initially deployed to so it's not included in the `vm.dumpState`.
 contract L2Genesis is Script, Artifacts {
-    uint256 constant PROXY_COUNT = 2048;
-    uint256 constant PRECOMPILE_COUNT = 256;
     DeployConfig public constant cfg =
         DeployConfig(address(uint160(uint256(keccak256(abi.encode("optimism.deployconfig"))))));
-
-    /// @notice The storage slot that holds the address of a proxy implementation.
-    /// @dev `bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)`
-    bytes32 internal constant PROXY_IMPLEMENTATION_ADDRESS =
-        0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-
-    /// @notice The storage slot that holds the address of the owner.
-    /// @dev `bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)`
-    bytes32 internal constant PROXY_ADMIN_ADDRESS = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
-    uint80 internal constant DEV_ACCOUNT_FUND_AMT = 10_000 ether;
-    /// @notice Default Anvil dev accounts. Only funded if `cfg.fundDevAccounts == true`.
-    address[10] internal devAccounts = [
-        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
-        0x70997970C51812dc3A010C7d01b50e0d17dc79C8,
-        0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC,
-        0x90F79bf6EB2c4f870365E785982E1f101E93b906,
-        0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65,
-        0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc,
-        0x976EA74026E726554dB657fA54763abd0C3a0aa9,
-        0x14dC79964da2C08b23698B3D3cc7Ca32193d9955,
-        0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f,
-        0xa0Ee7A142d267C1f36714E4a8F75612F20a79720
-    ];
 
     string internal outfile;
 
@@ -114,7 +90,7 @@ contract L2Genesis is Script, Artifacts {
     /// @notice Give all of the precompiles 1 wei so that they are
     ///         not considered empty accounts.
     function _dealEthToPrecompiles() internal {
-        for (uint256 i; i < PRECOMPILE_COUNT; i++) {
+        for (uint256 i; i < L2GenesisHelpers.PRECOMPILE_COUNT; i++) {
             vm.deal(address(uint160(i)), 1);
         }
     }
@@ -130,21 +106,21 @@ contract L2Genesis is Script, Artifacts {
         console.log(
             "Setting proxy deployed bytecode for addresses in range %s through %s",
             address(prefix | uint160(0)),
-            address(prefix | uint160(PROXY_COUNT - 1))
+            address(prefix | uint160(L2GenesisHelpers.PROXY_COUNT - 1))
         );
-        for (uint256 i = 0; i < PROXY_COUNT; i++) {
+        for (uint256 i = 0; i < L2GenesisHelpers.PROXY_COUNT; i++) {
             address addr = address(prefix | uint160(i));
-            if (_notProxied(addr)) {
+            if (L2GenesisHelpers.notProxied(addr)) {
                 continue;
             }
 
             vm.etch(addr, code);
-            vm.store(addr, PROXY_ADMIN_ADDRESS, bytes32(uint256(uint160(Predeploys.PROXY_ADMIN))));
+            vm.store(addr, EIP1967Helper.PROXY_OWNER_ADDRESS, bytes32(uint256(uint160(Predeploys.PROXY_ADMIN))));
 
-            if (_isDefinedPredeploy(addr)) {
-                address implementation = _predeployToCodeNamespace(addr);
+            if (L2GenesisHelpers.isDefinedPredeploy(addr)) {
+                address implementation = L2GenesisHelpers.predeployToCodeNamespace(addr);
                 console.log("Setting proxy %s implementation: %s", addr, implementation);
-                vm.store(addr, PROXY_IMPLEMENTATION_ADDRESS, bytes32(uint256(uint160(implementation))));
+                vm.store(addr, EIP1967Helper.PROXY_IMPLEMENTATION_ADDRESS, bytes32(uint256(uint160(implementation))));
             }
         }
     }
@@ -254,7 +230,7 @@ contract L2Genesis is Script, Artifacts {
             _withdrawalNetwork: FeeVault.WithdrawalNetwork(cfg.sequencerFeeVaultWithdrawalNetwork())
         });
 
-        address impl = _predeployToCodeNamespace(Predeploys.SEQUENCER_FEE_WALLET);
+        address impl = L2GenesisHelpers.predeployToCodeNamespace(Predeploys.SEQUENCER_FEE_WALLET);
         console.log("Setting %s implementation at: %s", "SequencerFeeVault", impl);
         vm.etch(impl, address(vault).code);
 
@@ -326,33 +302,8 @@ contract L2Genesis is Script, Artifacts {
         _setImplementationCode(Predeploys.L1_BLOCK_ATTRIBUTES, "L1Block");
     }
 
-    /// @dev Returns true if the address is not proxied.
-    function _notProxied(address _addr) internal pure returns (bool) {
-        return _addr == Predeploys.GOVERNANCE_TOKEN || _addr == Predeploys.WETH9;
-    }
-
-    /// @dev Returns true if the address is a predeploy.
-    function _isDefinedPredeploy(address _addr) internal pure returns (bool) {
-        return _addr == Predeploys.L2_TO_L1_MESSAGE_PASSER || _addr == Predeploys.L2_CROSS_DOMAIN_MESSENGER
-            || _addr == Predeploys.L2_STANDARD_BRIDGE || _addr == Predeploys.L2_ERC721_BRIDGE
-            || _addr == Predeploys.SEQUENCER_FEE_WALLET || _addr == Predeploys.OPTIMISM_MINTABLE_ERC20_FACTORY
-            || _addr == Predeploys.OPTIMISM_MINTABLE_ERC721_FACTORY || _addr == Predeploys.L1_BLOCK_ATTRIBUTES
-            || _addr == Predeploys.GAS_PRICE_ORACLE || _addr == Predeploys.DEPLOYER_WHITELIST || _addr == Predeploys.WETH9
-            || _addr == Predeploys.L1_BLOCK_NUMBER || _addr == Predeploys.LEGACY_MESSAGE_PASSER
-            || _addr == Predeploys.PROXY_ADMIN || _addr == Predeploys.BASE_FEE_VAULT || _addr == Predeploys.L1_FEE_VAULT
-            || _addr == Predeploys.GOVERNANCE_TOKEN || _addr == Predeploys.SCHEMA_REGISTRY || _addr == Predeploys.EAS;
-    }
-
-    /// @dev Function to compute the expected address of the predeploy implementation
-    ///      in the genesis state.
-    function _predeployToCodeNamespace(address _addr) internal pure returns (address) {
-        return address(
-            uint160(uint256(uint160(_addr)) & 0xffff | uint256(uint160(0xc0D3C0d3C0d3C0D3c0d3C0d3c0D3C0d3c0d30000)))
-        );
-    }
-
     function _setImplementationCode(address _addr, string memory _name) internal returns (address) {
-        address impl = _predeployToCodeNamespace(_addr);
+        address impl = L2GenesisHelpers.predeployToCodeNamespace(_addr);
         console.log("Setting %s implementation at: %s", _name, impl);
         vm.etch(impl, vm.getDeployedCode(string.concat(_name, ".sol:", _name)));
 
@@ -378,6 +329,7 @@ contract L2Genesis is Script, Artifacts {
     }
 
     /// @dev Helper function to sort the genesis alloc numerically by address.
+    /// @notice The storage slot keys are also sorted numerically.
     function _sortJsonByKeys(string memory _path) internal {
         string[] memory commands = new string[](3);
         commands[0] = "bash";
@@ -387,15 +339,17 @@ contract L2Genesis is Script, Artifacts {
             "to_entries | ",
             "map( {key: .key, value: {balance: .value.balance, code: .value.code, nonce: .value.nonce, storage: (.value.storage | to_entries | sort_by(.key) | from_entries)} } ) | ",
             "from_entries' ",
-            _path, ") > ", _path
+            _path,
+            ") > ",
+            _path
         );
         vm.ffi(commands);
     }
 
     function _fundDevAccounts() internal {
-        for (uint256 i; i < devAccounts.length; i++) {
-            console.log("Funding dev account %s with %s ETH", devAccounts[i], DEV_ACCOUNT_FUND_AMT / 1e18);
-            vm.deal(devAccounts[i], DEV_ACCOUNT_FUND_AMT);
+        for (uint256 i; i < cfg.getDevAccounts().length; i++) {
+            console.log("Funding dev account %s with %s ETH", cfg.getDevAccounts()[i], DEV_ACCOUNT_FUND_AMT / 1e18);
+            vm.deal(cfg.getDevAccounts()[i], DEV_ACCOUNT_FUND_AMT);
         }
 
         _checkDevAccountsFunded();
@@ -424,10 +378,12 @@ contract L2Genesis is Script, Artifacts {
     }
 
     function _checkDevAccountsFunded() internal view {
-        for (uint256 i; i < devAccounts.length; i++) {
-            if (devAccounts[i].balance != DEV_ACCOUNT_FUND_AMT) {
+        for (uint256 i; i < cfg.getDevAccounts().length; i++) {
+            if (cfg.getDevAccounts()[i].balance != DEV_ACCOUNT_FUND_AMT) {
                 revert(
-                    string.concat("Dev account not funded with expected amount of ETH: ", vm.toString(devAccounts[i]))
+                    string.concat(
+                        "Dev account not funded with expected amount of ETH: ", vm.toString(cfg.getDevAccounts()[i])
+                    )
                 );
             }
         }
--- packages/contracts-bedrock/test/scripts/L2Genesis.t.sol
+++ packages/contracts-bedrock/test/scripts/L2Genesis.t.sol
@@ -4,28 +4,32 @@ pragma solidity 0.8.15;
 import { Test } from "forge-std/Test.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { console2 as console } from "forge-std/console2.sol";
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 
 import { Artifacts } from "scripts/Artifacts.s.sol";
+import { DeployConfig, DEV_ACCOUNT_FUND_AMT } from "scripts/DeployConfig.s.sol";
 import { Executables } from "scripts/Executables.sol";
 import { Predeploys } from "src/libraries/Predeploys.sol";
-import { L2GenesisFixtures } from "test/fixtures/L2GenesisFixtures.sol";
+import { L2GenesisHelpers } from "scripts/libraries/L2GenesisHelpers.sol";
 
+/// @notice Reads a `genesis-l2.json` file, parses the `alloc`s, and runs assertions
+///         against each alloc depending on whether it's a precompile, predeploy proxy,
+///         or predeploy implementation.
 contract L2Genesis_Test is Test, Artifacts {
-    uint256 constant PRECOMPILE_COUNT = 256;
+    DeployConfig public constant cfg =
+        DeployConfig(address(uint160(uint256(keccak256(abi.encode("optimism.deployconfig"))))));
 
     string internal genesisPath;
-    string[] allocAddresses;
-    L2GenesisFixtures l2GenesisFixtures;
 
     struct StorageData {
         bytes32 key;
         bytes32 value;
     }
 
-    /// @notice `balance` and `nonce` are being parsed as `bytes` even though their JSON representations are hex strings.
+    /// @notice `balance` and `nonce` are being parsed as `bytes` even though their JSON representations are hex
+    /// strings.
     ///         This is because Foundry has a limitation around parsing strings as numbers when using `vm.parseJson`,
-    ///         and because we're using `abi.decode` to convert the JSON string, we can't use coersion (i.e. `vm.parseJsonUint`)
+    ///         and because we're using `abi.decode` to convert the JSON string, we can't use coersion (i.e.
+    /// `vm.parseJsonUint`)
     ///         to tell Foundry that the strings are numbers. So instead we treat them as `byte` strings and parse as
     ///         `uint`s when needed. Additional context: https://github.com/foundry-rs/foundry/issues/3754
     struct Alloc {
@@ -36,61 +40,139 @@ contract L2Genesis_Test is Test, Artifacts {
         StorageData[] storageData;
     }
 
-    mapping(address => StorageData[]) internal expectedAllocStorage;
-
+    /// @notice Checks if deploy config file exists at `deployConfigPath`, if it doesn't
+    ///         reverts with warning to run `make devnet-allocs` at monorepo root.
+    ///         Afterwards, checks if genesis file exists at `genesisPath`, and if not
+    ///         runs `L2Genesis.s.sol` script to generate it.
     function setUp() public override {
         super.setUp();
-
         Artifacts.setUp();
-        genesisPath = string.concat(vm.projectRoot(), "/deployments/", deploymentContext, "/genesis-l2.json");
 
-        l2GenesisFixtures = new L2GenesisFixtures();
-        l2GenesisFixtures.setUp();
+        string memory deployConfigPath = string.concat(vm.projectRoot(), "/deploy-config/", deploymentContext, ".json");
+        if (!vm.exists(deployConfigPath)) {
+            revert(
+                string.concat(
+                    "Did not find deploy config at: ",
+                    deployConfigPath,
+                    ", try running make devnet-allocs in monorepo root"
+                )
+            );
+        }
+
+        vm.etch(address(cfg), vm.getDeployedCode("DeployConfig.s.sol:DeployConfig"));
+        vm.label(address(cfg), "DeployConfig");
+        vm.allowCheatcodes(address(cfg));
+        cfg.read(deployConfigPath);
+
+        genesisPath = string.concat(vm.projectRoot(), "/deployments/", deploymentContext, "/genesis-l2.json");
+        if (!vm.exists(genesisPath)) {
+            string[] memory commands = new string[](3);
+            commands[0] = "bash";
+            commands[1] = "-c";
+            commands[2] = string.concat(
+                "DEPLOYMENT_CONTEXT=",
+                deploymentContext,
+                " forge script --chain-id ",
+                vm.toString(cfg.l2ChainID()),
+                " ./scripts/L2Genesis.s.sol:L2Genesis"
+            );
+            vm.ffi(commands);
+        }
     }
 
+    /// @notice Iterates over every alloc parsed from `genesisPath`, and depending on
+    ///         the alloc address, runs specific checks based on whether the alloc is
+    ///         a precompile, predeploy proxy, or predeploy implementation. If the
+    ///         alloc address doesn't pass any of the checks to determine what it is,
+    ///         the function reverts with
+    ///         `string.concat("Unknown alloc: ", Strings.toHexString(allocs[i].addr)`.
     function test_allocs() external {
         Alloc[] memory allocs = _parseAllocs(genesisPath);
 
-        for(uint256 i; i < allocs.length; i++) {
+        for (uint256 i; i < allocs.length; i++) {
             uint160 numericAddress = uint160(allocs[i].addr);
-            if (numericAddress < PRECOMPILE_COUNT) {
+            if (numericAddress < L2GenesisHelpers.PRECOMPILE_COUNT) {
                 _checkPrecompile(allocs[i]);
             } else if (_isProxyAddress(allocs[i].addr)) {
                 _checkProxy(allocs[i]);
             } else if (_isImplementationAddress(allocs[i].addr)) {
-                // console.log("Implementation: %s", allocs[i].addr);
+                _checkImplementation(allocs[i]);
+            } else if (_isDevAccount(allocs[i].addr)) {
+                _checkDevAccount(allocs[i]);
             } else {
-                revert(string.concat("Unknown alloc: ", Strings.toHexString(allocs[i].addr)));
+                revert(string.concat("Unknown alloc: ", vm.toString(allocs[i].addr)));
             }
         }
     }
 
+    /// @notice Runs checks against `_alloc` to determine if it's an expected precompile.
+    ///         The following should hold true for every precompile:
+    ///         1. The alloc should have a balance of `1`.
+    ///         2. The alloc should not have `code` set.
+    ///         3. The alloc should have a `nonce` of `0`.
+    ///         4. The alloc should not have any storage slots set.
     function _checkPrecompile(Alloc memory _alloc) internal {
-        assertEq(_alloc.balance, hex'01');
-        assertEq(_alloc.code, hex'');
-        assertEq(_alloc.nonce, hex'00');
+        assertEq(_alloc.balance, hex"01");
+        assertEq(_alloc.code, hex"");
+        assertEq(_alloc.nonce, hex"00");
         assertEq(_alloc.storageData.length, 0);
     }
 
+    /// @notice Runs checks against `_alloc` to determine if it's an expected predeploy proxy.
+    ///         The following should hold true for every predeploy proxy:
+    ///         1. The alloc should have a balance of `0`.
+    ///         2. The alloc should have `code` set `Proxy.sol` deployed bytecode.
+    ///         3. The alloc should have a `nonce` of `0`.
+    ///         4. The alloc should two storage slots set:
+    ///            1. L2GenesisHelpers.PROXY_ADMIN_ADDRESS
+    ///            2. L2GenesisHelpers.PROXY_IMPLEMENTATION_ADDRESS
     function _checkProxy(Alloc memory _alloc) internal {
-        assertEq(_alloc.balance, hex'00');
-        assertEq(_alloc.nonce, hex'00');
+        assertEq(_alloc.balance, hex"00");
+        assertEq(_alloc.nonce, hex"00");
+        assertNotEq(_alloc.storageData.length, 0);
 
-        if (!_notProxied(_alloc.addr)) {
+        if (!L2GenesisHelpers.notProxied(_alloc.addr)) {
             assertEq(_alloc.code, vm.getDeployedCode("Proxy.sol:Proxy"));
-        }
-
-        /// First we assert we have the same number of set storage slots for `_alloc` that we expect to be set.
-        assertEq(_alloc.storageData.length, l2GenesisFixtures.getNumExpectedSlotKeys(_alloc.addr));
-        /// Then we loop through all of `_alloc`'s storage slots and check if that storage slot is supposed be set,
-        /// lastly we assert that the corresponding slot value matches what's expected for that slot.
-        for (uint256 i; i < _alloc.storageData.length; i++) {
-            assertTrue(l2GenesisFixtures.isExpectedSlotKey(_alloc.addr, _alloc.storageData[i].key));
-            assertEq(_alloc.storageData[i].value, l2GenesisFixtures.getSlotValueByKey(_alloc.addr, _alloc.storageData[i].key));
         }
     }
 
-    function _parseAllocs(string memory _filePath) internal returns(Alloc[] memory) {
+    /// @notice Runs checks against `_alloc` to determine if it's an expected predeploy implementation.
+    ///         The following should hold true for every predeploy implementation:
+    ///         1. The alloc should have a balance of `0`.
+    ///         2. The alloc should have `code` set to it's respective predeploy deployed bytecode.
+    ///         3. The alloc should have a `nonce` of `0`.
+    ///         4. The alloc should have the storage slots set according to the specifc predeploy.
+    function _checkImplementation(Alloc memory _alloc) internal {
+        assertEq(_alloc.balance, hex"00");
+        assertEq(_alloc.nonce, hex"00");
+        assertNotEq(keccak256(_alloc.code), keccak256(bytes("0x")));
+
+        if (_hasStorageSlotsSet(_alloc.addr)) {
+            assertNotEq(_alloc.storageData.length, 0);
+        } else {
+            assertEq(_alloc.storageData.length, 0);
+        }
+    }
+
+    function _checkDevAccount(Alloc memory _alloc) internal {
+        if (!cfg.fundDevAccounts()) {
+            revert(
+                string.concat(
+                    "Dev account ",
+                    vm.toString(_alloc.addr),
+                    " present in allocs, but fundDevAccounts is not enabled in deploy config"
+                )
+            );
+        }
+
+        assertEq(vm.parseUint(vm.toString(_alloc.balance)), DEV_ACCOUNT_FUND_AMT);
+        assertEq(_alloc.code, hex"");
+        assertEq(_alloc.nonce, hex"00");
+        assertEq(_alloc.storageData.length, 0);
+    }
+
+    /// @notice Parses a given `_filePath` into a `Alloc[]`.
+    function _parseAllocs(string memory _filePath) internal returns (Alloc[] memory) {
         string[] memory cmd = new string[](3);
         cmd[0] = "bash";
         cmd[1] = "-c";
@@ -104,12 +186,14 @@ contract L2Genesis_Test is Test, Artifacts {
         return abi.decode(parsedJson, (Alloc[]));
     }
 
-    function _addressHasPrefix(address _addr, uint160 _prefix, uint160 _mask) internal pure returns(bool) {
+    /// @notice Returns whether the given address has a given prefix.
+    function _addressHasPrefix(address _addr, uint160 _prefix, uint160 _mask) internal pure returns (bool) {
         uint160 numericAddress = uint160(_addr);
         return (numericAddress & _mask) == _prefix;
     }
 
-    function _isProxyAddress(address _addr) internal pure returns(bool) {
+    /// @notice Returns whether a given address has the expected predeploy proxy prefix.
+    function _isProxyAddress(address _addr) internal pure returns (bool) {
         return _addressHasPrefix(
             _addr,
             uint160(0x4200000000000000000000000000000000000000),
@@ -117,7 +201,8 @@ contract L2Genesis_Test is Test, Artifacts {
         );
     }
 
-    function _isImplementationAddress(address _addr) internal pure returns(bool) {
+    /// @notice Returns whether a given address has the expected predeploy implementation prefix.
+    function _isImplementationAddress(address _addr) internal pure returns (bool) {
         return _addressHasPrefix(
             _addr,
             uint160(0xc0D3C0d3C0d3C0D3c0d3C0d3c0D3C0d3c0d30000),
@@ -125,7 +210,23 @@ contract L2Genesis_Test is Test, Artifacts {
         );
     }
 
-    function _notProxied(address _addr) internal pure returns(bool) {
-        return _addr == Predeploys.GOVERNANCE_TOKEN || _addr == Predeploys.WETH9;
+    function _isDevAccount(address _addr) internal pure returns (bool) {
+        for (uint256 i; i < cfg.getDevAccounts().length; i++) {
+            if (_addr == cfg.getDevAccounts()[i]) return true;
+        }
+
+        return false;
+    }
+
+    function _hasStorageSlotsSet(address _addr) internal pure returns (bool) {
+        address addr = _addr;
+        if (_isImplementationAddress(addr)) {
+            uint16 lastTwoBytes = uint16(uint160(addr));
+            addr =
+                address(uint160((uint256(lastTwoBytes) | uint256(uint160(0x4200000000000000000000000000000000000000)))));
+        }
+
+        return addr == Predeploys.L2_CROSS_DOMAIN_MESSENGER || addr == Predeploys.L2_STANDARD_BRIDGE
+            || addr == Predeploys.OPTIMISM_MINTABLE_ERC20_FACTORY;
     }
 }
